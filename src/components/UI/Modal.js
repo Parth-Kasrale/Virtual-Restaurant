243ea9cc507120d2df00ecd0b8e55f1235e48371
parth kasrale <parthkasrale1@gmail.com>
2023-01-10 17:37:41 +0530

    Modified src/components/UI/Modal.js

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
-import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={props.onClose} />;
@@ -14,16 +15,18 @@ const ModalOverlay = (props) => {
   );
 };
 
+const portalElement = document.getElementById("overlays");
+
 const Modal = (props) => {
   return (
     <Fragment>
       {ReactDOM.createPortal(
         <Backdrop onClose={props.onClose} />,
-        document.getElementById("overlays")
+        portalElement
       )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
-        document.getElementById("overlays")
+        portalElement
       )}
     </Fragment>
   );
